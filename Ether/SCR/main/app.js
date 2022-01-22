359eb89b5bee2fe19d4620cb49c8d9d56be4163b
Adam Duda <97346707+AdamDuda1@users.noreply.github.com>
2022-01-22 14:30:08 +0100

    Fixed bugs

--- Ether/SCR/main/app.js
+++ Ether/SCR/main/app.js
@@ -1,15 +1,22 @@
-const change_launguage_button = document.querySelector('.change_lang');
+const change_launguage_button = document.querySelector('.footerButton');
+let lang = localStorage.getItem("lang");
 
 function setup() {
     console.log("The page has loaded successfully.");
     localStorage.setItem("lang", "en");
 
-    let lang = localStorage.getItem("lang");
     if (lang = "en") {
         console.log(".localStorage has loaded successfully.");
     }
+    console.log("Your language: " + localStorage.getItem("lang") + ".")
 }
 
 function change_lang() {
-
+    if (lnag = "en") {
+        localStorage.setItem("lang", "pl");
+    }
+    else if (lang = "pl") {
+        localStorage.setItem("lang", "en");
+    }
+    console.log("Your language: " + localStorage.getItem("lang") + ".")
 }
