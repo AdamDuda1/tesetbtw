7fd035a21ab0573e2e32d22b8c94ef127576341b
Adam Duda <97346707+AdamDuda1@users.noreply.github.com>
2022-01-24 08:19:23 +0100

    xddxxxxxxxddddddddddx

--- Ether/jQuery.js
+++ Ether/jQuery.js
@@ -3,7 +3,7 @@
     "use strict";
     "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function(e) {
         if (!e.document)
-            throw new Error("jQuery requires a window with a document");
+            throw new Error("We requires a window with a document");
         return t(e)
     }
     : t(e)
